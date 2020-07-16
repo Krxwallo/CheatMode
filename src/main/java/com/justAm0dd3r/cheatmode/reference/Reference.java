358bd5fea7f4200e6e6d7929fcbad595dcd7cba3
Krxwallo <a@b.c>
2020-07-16 16:28:42 +0200

    Initial Commit

---
+++ src/main/java/com/justAm0dd3r/cheatmode/reference/Reference.java
@@ -0,0 +1,8 @@
+package com.justAm0dd3r.cheatmode.reference;
+
+public class Reference {
+    public static final String MOD_ID = "cheat_mode";
+    public static final String MOD_NAME = "Cheat Mode Mod";
+    public static final String AUTHOR = "justAm0dd3r";
+    public static final String VERSION = "1.0.0";
+}
