a15b4f5fe36bd6f8f4007ec20214014102a6120f
Krxwallo <a@b.c>
2020-07-23 17:10:06 +0200

    "Update" 1.1.0

--- src/main/java/com/justAm0dd3r/cheatmode/reference/Reference.java
+++ src/main/java/com/justAm0dd3r/cheatmode/reference/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
     public static final String MOD_ID = "cheat_mode";
     public static final String MOD_NAME = "Cheat Mode Mod";
     public static final String AUTHOR = "justAm0dd3r";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 }
