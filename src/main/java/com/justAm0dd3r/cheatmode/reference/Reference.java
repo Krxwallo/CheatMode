a3d1082fe9e1fb26695a36fd291ce5f054642012
Krxwallo <a@b.c>
2020-11-21 14:36:19 +0100

    Fixed only working for 1.16.1 and fixed config file not getting generated.

--- src/main/java/com/justAm0dd3r/cheatmode/reference/Reference.java
+++ src/main/java/com/justAm0dd3r/cheatmode/reference/Reference.java
@@ -7,5 +7,4 @@ public class Reference {
     public static final String MOD_ID = "cheat_mode";
     public static final String MOD_NAME = "Cheat Mode Mod";
     public static final String AUTHOR = "justAm0dd3r";
-    public static final String VERSION = "1.1.0";
 }
