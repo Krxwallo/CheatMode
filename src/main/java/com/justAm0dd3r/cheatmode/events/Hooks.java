3590a694005cd8ec3e5d77bfe1b7cfc97028be99
Krxwallo <59575572+Krxwallo@users.noreply.github.com>
2022-09-01 11:39:24 +0200

    :lipstick: render tooltip for ItemButton

--- src/main/java/com/justAm0dd3r/cheatmode/events/Hooks.java
+++ src/main/java/com/justAm0dd3r/cheatmode/events/Hooks.java
@@ -2,6 +2,7 @@ package com.justAm0dd3r.cheatmode.events;
 
 import com.justAm0dd3r.cheatmode.config.Config;
 import com.justAm0dd3r.cheatmode.gui.button.ItemButton;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
@@ -12,12 +13,16 @@ import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.GameType;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
-
+@Mod.EventBusSubscriber
 public final class Hooks {
+    private static ItemButton button;
 
     private static GameType previousGameMode = GameType.SURVIVAL;
     public static boolean open = false; // "cheat mode screen" open?
@@ -56,7 +61,7 @@ public final class Hooks {
         if (screen instanceof InventoryScreen invScreen) {
             if (Config.CLIENT.instantCreativeInventory.get()) creative();
             else {
-                ItemButton button = new ItemButton(invScreen.getGuiLeft() + 77, invScreen.getGuiTop() + 30, b -> creative());
+                button = new ItemButton(invScreen.getGuiLeft() + 77, invScreen.getGuiTop() + 30, b -> creative());
                 addButton(invScreen, button);
             }
         }
@@ -86,6 +91,15 @@ public final class Hooks {
                 // Server -> require op
                 if (!mc().player.hasPermissions(2)) mc().player.sendSystemMessage(Component.translatable("message.cheatmode.no_permissions").withStyle(ChatFormatting.RED));
                 else mc().player.commandUnsigned("gamemode " + previousGameMode.name().toLowerCase());
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onScreenDrawPost(ScreenEvent.Render.Post event) {
+        if (event.getScreen() instanceof InventoryScreen screen && button != null) {
+            if(button.isMouseOver(event.getMouseX(), event.getMouseY())) {
+                screen.renderTooltip(new PoseStack(), Component.translatable("gui.cheatmode.open_creative_inventory"), event.getMouseX(), event.getMouseY());
             }
         }
     }
