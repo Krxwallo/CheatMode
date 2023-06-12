a6fc3b5e50025104a4e1dc1cc8da89daae42ed7d
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-06-12 20:14:18 +0200

    Update to Minecraft 1.20 (ver. 2.1.0-alpha)

--- src/main/java/com/justAm0dd3r/cheatmode/gui/button/ItemButton.java
+++ src/main/java/com/justAm0dd3r/cheatmode/gui/button/ItemButton.java
@@ -1,9 +1,7 @@
 package com.justAm0dd3r.cheatmode.gui.button;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.network.chat.ComponentContents;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
@@ -16,16 +14,14 @@ public class ItemButton extends Button {
     }
 
     @Override
-    public void renderWidget(@NotNull PoseStack stack, int mouseX, int mouseY, float partialTicks) {
-        renderButton(stack);
-        super.renderWidget(stack, mouseX, mouseY, partialTicks);
+    public void renderWidget(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        renderButton(graphics);
+        super.renderWidget(graphics, mouseX, mouseY, partialTicks);
     }
 
-    public void renderButton(PoseStack stack) {
-        ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
-
+    public void renderButton(GuiGraphics graphics) {
         this.alpha = 200.0F;
-        renderer.renderAndDecorateItem(stack, new ItemStack(Blocks.GRASS_BLOCK), getX(), getY());
+        graphics.renderItem(new ItemStack(Blocks.GRASS_BLOCK), getX(), getY());
         this.alpha = 0.0F;
     }
 }
